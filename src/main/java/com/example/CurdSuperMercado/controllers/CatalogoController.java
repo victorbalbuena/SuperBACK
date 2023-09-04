98414494f20bf448093742321085a5bfed982efb
Victor Balbuena <balbuenabellovictorantonio@gmail.com>
2023-09-03 22:18:54 -0600

    completed

--- src/main/java/com/example/CurdSuperMercado/controllers/CatalogoController.java
+++ src/main/java/com/example/CurdSuperMercado/controllers/CatalogoController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,6 +32,12 @@ public class CatalogoController {
     @CrossOrigin
     @PostMapping("/alta")
     public CatalogoModel guardarModel(@RequestBody CatalogoModel catalogo) {
+        return this.catalogoService.guardarCatalogo(catalogo);
+    }
+
+    @CrossOrigin
+    @PutMapping("/modificacion")
+    public CatalogoModel modificarModel(@RequestBody CatalogoModel catalogo) {
         return this.catalogoService.guardarCatalogo(catalogo);
     }
 
